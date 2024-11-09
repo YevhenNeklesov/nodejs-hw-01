b32fd3e05586e43b38b555521f66b2f37f4c5542
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2024-11-09 22:15:10 +0100

    fix allcontact

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -3,11 +3,12 @@ import { writeContacts } from "../utils/writeContacts.js";
 
 export const removeAllContacts = async () => {
     const allContacts = await readContacts();
-    if (allContacts.length == 0) {
-        console.log('Не має контактів для видалення');
-    } else {
+
+    if (allContacts.length > 0) {
         await writeContacts([]);
         console.log('Всі контакти успішно видалені!');
+    } else {
+        console.log('Не має контактів для видалення');
     }
 };
 
